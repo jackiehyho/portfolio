2ffc4759a46ab821a4331a20aab8cf55706fa842
jackiehyho <jackie.hy.ho@gmail.com>
2021-01-27 19:07:26 -0800

    Styled hero section

--- src/components/container.js
+++ src/components/container.js
@@ -4,7 +4,10 @@ import Styled from '@emotion/styled'
 const StyledContainer = Styled.div`
 max-width: 1366px;
 margin: 0 auto;
+height: ${props =>
+  props.full ? '100vh' : 'initial'};
 `
+
 export default ({ children }) => (
   <StyledContainer>
     {children}
